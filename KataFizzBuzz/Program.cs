db6e65c25f36e43a36688b3b30bf6418ee5d6830
HuberT <jgarcialanzas@gmail.com>
2018-05-29 18:48:09 +0200

    Add files via upload

---
+++ KataFizzBuzz/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataFizzBuzz
+{
+    public class KataFizzBuzz
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 0 ; x<=100; x++)
+            {
+                
+                Console.WriteLine( x + " -> " + fizzBuzz(x) );
+   
+            }
+            String name = Console.ReadLine();
+        }
+        public static string fizzBuzz(int x)
+        {
+            if (x % 15 == 0)
+            {
+                return "FizzBuzz";
+
+            }
+            else if (x % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else if (x % 5 == 0)
+            {
+                return "Buzz";
+            }
+
+            return x.ToString();
+        }
+    }
+
+
+
+    /*
+                 if(x % 3 == 0 && x % 5 == 0)
+            {
+                return "FizzBuzz";
+            }else if(x % 3 == 0)
+            {
+                return "Fizz";
+            }else if(x % 5 == 0)
+            {
+                return "Buzz";
+            }
+           
+
+            return x.ToString();
+     
+     */
+
+}

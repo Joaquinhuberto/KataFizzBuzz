ad2407f46959bf5dbcaa26e231382b381506de03
Joaquin Garcia Lanzas <joaquingarcialanzas@macbook-pro.home>
2018-05-29 22:08:26 +0200

    Refactor TestFizzBuzz

--- TestFizzBuzz/Properties/AssemblyInfo.cs
+++ TestFizzBuzz/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("UnitTestKataFIzzBuzz")]
+[assembly: AssemblyTitle("TestFizzBuzz")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UnitTestKataFIzzBuzz")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("TestFizzBuzz")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("5de0638d-a0a5-4d11-a084-62893177b521")]
+[assembly: Guid("872fb82f-03b4-44fc-8111-b894afa3a982")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
---
+++ TestFizzBuzz/TestFizzBuzz.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestKataFIzzBuzz
+
+{
+    [TestClass]
+    public class TestFizzBuzz
+    {
+
+        [TestMethod]
+        public void FizzBuzz()
+        {
+            for (var x = 1; x <= 1000; x++)
+            {
+                if (x % 15 == 0)
+                {
+                    Assert.AreEqual("FizzBuzz", KataFizzBuzz.KataFizzBuzz.fizzBuzz(x));
+                }else if (x % 3 == 0)
+                {
+                    Assert.AreEqual("Fizz", KataFizzBuzz.KataFizzBuzz.fizzBuzz(x));
+                }else if (x % 5 == 0)
+                {
+                    Assert.AreEqual("Buzz", KataFizzBuzz.KataFizzBuzz.fizzBuzz(x));
+                }
+                else
+                {
+                    Assert.AreEqual(x.ToString(), KataFizzBuzz.KataFizzBuzz.fizzBuzz(x));
+                }
+            }
+        }
+    }
+}
